0f5b9944c71f713f9911255dad76ff07aa24958b
Francisco Gomes <1250944@isep.ipp.pt>
2025-10-12 11:40:17 +0100

    Update Divisores.java

--- APROG/Semana1/Divisores.java
+++ APROG/Semana1/Divisores.java
@@ -1,7 +1,7 @@
 package org.example;
 import java.util.Scanner;
 
-public class Main {
+public class DIvisores {
     public static void main(String[] arg){
         Scanner input = new Scanner(System.in);
         int numero = input.nextInt();
@@ -19,4 +19,5 @@ public class Main {
     }
 
 }
+
 
