8d2867252671ecf07d19c32835f688dcab3b9a29
Francisss9 <fpsgomes9@gmail.com>
2025-10-14 13:30:16 +0100

    Added new file with exercice K

---
+++ APROG/Semana2/Exercicio_K.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Exercicio_K {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+    }
+}
