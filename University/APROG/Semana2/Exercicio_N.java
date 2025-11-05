40997f06a4821c2e5e74ebb4b9c6ce6e2f70a7a4
Francisgomesx <80554120+Francisss9@users.noreply.github.com>
2025-11-05 22:59:13 +0000

    Update Exercicio_N.java

--- University/APROG/Semana2/Exercicio_N.java
+++ University/APROG/Semana2/Exercicio_N.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Exercicio_N {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int previous = -1; // no previous number initially
+        int previous = -1;
         boolean keepReading = true;
 
         while (keepReading) {
             int number = sc.nextInt();
 
-            if (number < 0) { // stop reading on negative number
+            if (number < 0) {
                 keepReading = false;
             } else {
                 if (previous != -1 && number > previous && digitsAscending(number)) {
@@ -22,7 +22,6 @@ public class Exercicio_N {
         sc.close();
     }
 
-    // function to check if digits of a number are in ascending order
     public static boolean digitsAscending(int number) {
         String numStr = Integer.toString(number);
         for (int i = 0; i < numStr.length() - 1; i++) {
