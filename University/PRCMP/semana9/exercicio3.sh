30d4610ac8aef6576ada16570851c4e5358408af
Francisco Gomes <1250944@isep.ipp.pt>
2026-02-07 11:25:15 +0000

    Fix file renaming logic in exercicio3.sh

--- University/PRCMP/semana9/exercicio3.sh
+++ University/PRCMP/semana9/exercicio3.sh
@@ -14,10 +14,18 @@ fi
 
 cd "$directory" || exit 1
 
+if ! ls *.dat >/dev/null 2>&1; then
+  echo "Não há ficheiros .dat no diretório."
+  exit 0
+fi
+
 contador=0
 
 for filename in *.dat; do
-  contador+=1
-  echo ""
-
+  contador=$((contador + 1))
+  new_filename="${contador}-${filename}"
+  mv -- "$filename" "$new_filename"
+  echo "$new_filename"
 done
+
+echo "Foram renomeados $contador ficheiros."
