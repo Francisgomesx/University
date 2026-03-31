65ebdb41ded7bf4a30aea218ac273e7dbba357f6
Francisgomesx <1250944@isep.ipp.pt>
2026-03-31 09:14:27 +0100

    Finished making the unitary tests for the exercice 'VencimentosComComparable'

--- University/PPROG/Semana5/VencimentosComComparable/src/test/java/TrabalhadorTest.java
+++ University/PPROG/Semana5/VencimentosComComparable/src/test/java/TrabalhadorTest.java
@@ -21,8 +21,30 @@ public class TrabalhadorTest {
     }
 
     @Test
-    void testCalcularVencimentoTrabalhadorComissao() {
+    public void testCalcularVencimentoTrabalhadorComissao() {
         TrabalhadorComissao t = new TrabalhadorComissao("Miguel Oliveira", 1300.40f, 4.25f, 2731.50f);
         assertEquals(1416.48875, t.calcularVencimento(), 0.001);
     }
+
+    @Test
+    public void testCompareToVencimentoMenor() {
+        TrabalhadorHora t1 = new TrabalhadorHora("Carlos", 100, 4.50f);
+        TrabalhadorHora t2 = new TrabalhadorHora("Ana", 100, 10.0f);
+        assertTrue(t1.compareTo(t2) < 0);
+    }
+
+    @Test
+    public void testCompareToVencimentoMaior() {
+        TrabalhadorHora t1 = new TrabalhadorHora("Carlos", 100, 10.0f);   // 1000
+        TrabalhadorHora t2 = new TrabalhadorHora("Ana", 100, 4.5f);       // 450
+        assertTrue(t1.compareTo(t2) > 0);
+    }
+
+    @Test
+    public void testCompareToVencimentoIgual() {
+        TrabalhadorHora t1 = new TrabalhadorHora("Carlos", 100, 5.0f);    // 500
+        TrabalhadorHora t2 = new TrabalhadorHora("Ana", 50, 10.0f);       // 500
+        assertEquals(0, t1.compareTo(t2));
+    }
+
 }
